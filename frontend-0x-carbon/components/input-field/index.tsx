7205a6457f6e5d94892c6a5e53a16b1259711344
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-03-15 21:18:48 -0300

    First commit: starting project just with a few configs

---
+++ frontend-0x-carbon/components/input-field/index.tsx
@@ -0,0 +1,7 @@
+const Input = () => {
+    return (
+        <input className="focus:border-blue-500" placeholder="Going to"></input>
+    )
+}
+
+export default Input
---
+++ frontend-0x-carbon/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Input from '../components/input-field'
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1>Expedia</h1>
+      <Input />
+    </>
+  )
+}
+
+export default Home
