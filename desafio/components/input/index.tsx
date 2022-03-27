85c60b757340c898b028c6f649a746a25f6a3456
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-03-27 00:14:50 -0300

    added icon on input

--- desafio/components/input/index.tsx
+++ desafio/components/input/index.tsx
@@ -1,6 +1,10 @@
+import { LocationMarkerIcon } from "@heroicons/react/outline"
 const Input = () => {
     return (
-        <input className="focus:border-blue-500" placeholder="Going to"></input>
+        <div className="flex">
+            <LocationMarkerIcon className="w-5 h-5" />
+            <input className="focus:border-blue-500" placeholder="Going to"></input>
+        </div>
     )
 }
 
