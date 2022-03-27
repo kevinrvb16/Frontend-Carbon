cb75256b266808056189cc01ec6917e2efbe18bd
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-03-27 00:14:00 -0300

    adjusted some button elements

--- desafio/components/button/index.tsx
+++ desafio/components/button/index.tsx
@@ -1,8 +1,7 @@
 const Button = () => {
     return (
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Search
-        </button>
+        <button className="m-4 inline-flex items-center py-2 px-3 font-medium text-base text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+
     )
 }
 
