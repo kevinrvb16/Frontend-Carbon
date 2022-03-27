88745d35acae18211b15c28a00f92b06742a8ca6
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-03-27 00:14:17 -0300

    adjusted some header elements

--- desafio/components/header/index.tsx
+++ desafio/components/header/index.tsx
@@ -2,7 +2,7 @@ import Navbar from "../navbar"
 
 const Header = () => {
     return (
-        <header className=" ml-3 container-header border-b border-transparent border-gray-300">
+        <header className=" container-header border-b border-gray-300">
             <Navbar/>
         </header>
     )
