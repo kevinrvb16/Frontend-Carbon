7db1f86268ec1166bcdc4eb3081ece144029765a
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-03-27 10:18:34 -0300

    created card-travel component

---
+++ desafio/components/card-travel/index.tsx
@@ -0,0 +1,16 @@
+const CardTravel = () => {
+    return (
+        <div className="max-w-xl bg-white  border-gray-200 ">
+        <a href="#">
+            <img className="rounded-t-lg" src="travel.jpg" alt="travel"/>
+        </a>
+        <div className="p-2">
+            <p className="mb-2 font-light text-xs text-gray-800 ">MADE TO TRAVEL</p>
+            <a href="#">
+                <h5 className="mb-2 text-base font-bold tracking-tight text-gray-900 ">It's just so nice out there. So let's go</h5>
+            </a>
+        </div>
+    </div>
+    )
+}
+export default CardTravel
