ced5c39f0c4fea7b47c212e12aec1f1cc2458627
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-03-28 09:58:18 -0300

    index page almost finished

--- desafio/pages/index.tsx
+++ desafio/pages/index.tsx
@@ -1,20 +1,24 @@
 import type { NextPage } from 'next'
 import Card from '../components/card'
+import FloatButton from '../components/float-button'
 import Grid2Col from '../components/grid-2-col'
 import Grid3Col from '../components/grid-3-col'
 import Header from '../components/header'
+import HorizontalCard from '../components/horizontal-card'
 import ImgCard from '../components/img-card'
 import Title from '../components/title'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="relative">
       <Header/>
       <Card/>
       <ImgCard/>
       <Title/>
       <Grid3Col/>
       <Grid2Col/>
+      <HorizontalCard/>
+      <FloatButton/>
     </div>
   )
 }
