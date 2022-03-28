d2a01f8f231b55c1028cee43d852558d0af14d35
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-03-28 09:55:30 -0300

    button size adjusted

--- desafio/components/button/index.tsx
+++ desafio/components/button/index.tsx
@@ -1,7 +1,6 @@
 const Button = () => {
     return (
-        <button className="m-4 inline-flex items-center py-2 px-3 font-medium text-base text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-
+        <button className="m-4 w-36  items-center py-2 px-3 font-medium text-base text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
     )
 }
 
