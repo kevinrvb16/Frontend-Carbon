94f80509abf1329772da9af1c8a772dc28117018
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2022-03-28 09:56:07 -0300

    search input added

---
+++ desafio/components/search-input/index.tsx
@@ -0,0 +1,12 @@
+const SearchInput = () => {
+    return (
+        <div className="m-0">
+            <div className="container flex">
+                <div className="relative"> <input type="text" className="h-12 w-72 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none" placeholder="e.g. Florianopolis"/>
+                    <div className="absolute top-4 right-3"> <i className="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i> </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default SearchInput
